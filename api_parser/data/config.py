29ed392ab1e95f34420a1525ab507a8817616108
Фурман Илья Дмитриевич <i.d.furman@tinkoff.ru>
2025-03-03 22:25:04 +0300

    Change format to reverse

--- api_parser/data/config.py
+++ api_parser/data/config.py
@@ -9,19 +9,8 @@ env.read_env()
 
 BASE_DIR: pathlib.Path = Path(env.str("BASE_DIR"))
 
-GOOGLE_TABLE_URL: str = env.str("GOOGLE_TABLE_URL")
-
 CIAN_API_URL: str = env.str("CIAN_API_URL")
-
-CIAN_ONE_TIME_TOKEN = env.str("CIAN_ONE_TIME_TOKEN")
-
-if not CIAN_ONE_TIME_TOKEN:
-    CIAN_ONE_TIME_TOKEN = None
-    ACCESS_TOKEN = env.str("ACCESS_TOKEN")
-    REFRESH_TOKEN = env.str("REFRESH_TOKEN")
-else:
-    ACCESS_TOKEN = None
-    REFRESH_TOKEN = None
+ACCESS_TOKEN = env.str("ACCESS_TOKEN")
 
 LOGGING_LEVEL: int = env.int("LOGGING_LEVEL", 10)
 
